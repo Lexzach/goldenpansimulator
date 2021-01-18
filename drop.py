acd9523d19935f3e8119035ec9a502b4c31cd563
Lexzach <53662039+Lexzach@users.noreply.github.com>
2021-01-18 16:08:32 -0500

    v1.2

--- drop.py
+++ drop.py
@@ -2,7 +2,6 @@ import random
 from random import randint
 import numpy
 
-price = 1.01
 tourNumber = 0
 totalSpent = 0
 panDropped = 0
@@ -10,26 +9,37 @@ simsRan = 0
 toursTotal = 0
 lowestTour = 9999999999999999999999
 odds = 0.0
-currentPanPrice = 4200
-print("Enter number of simulations to run: ")
-x = input()
 profit = 0
 loss = 0
-while simsRan != int(x):
+
+print("v1.2 - Made by Lexzach")
+print("\nEnter number of simulations to run: ")
+toursToRun = input()
+print("Enter number of tickets required for a tour: ")
+ticketsPerTour = input()
+print("Enter ticket price (current is $1.01 on Steam Marketplace):")
+price = input()
+price = price.replace("$", "")
+print("Enter golden pan price (current is $4200 on Marketplace.tf): ")
+currentPanPrice = input()
+currentPanPrice = currentPanPrice.replace("$", "")
+price = float(price)
+currentPanPrice = float(currentPanPrice)
+while simsRan != int(toursToRun):
     tourNumber = 0
     totalSpent = 0
     panDropped = 0
     while panDropped != 1:
         tourNumber+=1
-        totalSpent=totalSpent + (price*4)
+        totalSpent+=float(price)*float(ticketsPerTour)
         #print("Tour Number: "+str(tourNumber)+"\nTotal spent: "+str(totalspent))
         if randint(0,10000) == 10000:
             panDropped = 1
-    if tourNumber*(price*4) < currentPanPrice:
-        print("\nSimulation #"+str(simsRan)+"\nFinal Numbers:\nMoney spent - "+str(round(totalSpent, 2))+"\nTour Number - "+str(tourNumber)+"\nMoney spent on " + str(tourNumber) + " tours: $"+str(round(float(tourNumber*(price*4))))+", you made $" + str(currentPanPrice - tourNumber*(price*4)) + " in profit.")
+    if tourNumber*(price*int(ticketsPerTour)) < currentPanPrice:
+        print("\nSimulation #"+str(simsRan)+"\nMoney spent: "+str(round(totalSpent, 2))+"\nTour pan dropped: "+str(tourNumber)+"\nMoney spent on " + str(tourNumber) + " tours: $"+str(round(float(tourNumber*(price*int(ticketsPerTour)))))+", you made $" + str(currentPanPrice - tourNumber*(price*int(ticketsPerTour))) + " in profit.")
         profit+=1
     else:
-        print("\nSimulation #"+str(simsRan)+"\nFinal Numbers:\nMoney spent - "+str(round(totalSpent, 2))+"\nTour Number - "+str(tourNumber)+"\nMoney spent on " + str(tourNumber) + " tours: $"+str(round(float(tourNumber*(price*4))))+", you lost $" + str(currentPanPrice - tourNumber*(price*4)) + " in profit.")
+        print("\nSimulation #"+str(simsRan)+"\nMoney spent: "+str(round(totalSpent, 2))+"\nTour pan dropped: "+str(tourNumber)+"\nMoney spent on " + str(tourNumber) + " tours: $"+str(round(float(tourNumber*(price*int(ticketsPerTour)))))+", you lost $" + str(currentPanPrice - tourNumber*(price*int(ticketsPerTour))) + " in profit.")
         loss+=1
     if tourNumber < lowestTour:
         lowestTour=tourNumber
@@ -38,9 +48,9 @@ while simsRan != int(x):
 
 odds = numpy.random.binomial(lowestTour, 0.01, None)
 cost = round(float(lowestTour*(price*4)))
-if round(float(lowestTour*(price*4))) < currentPanPrice:
-    print("\nSim finished!\n\nTotal tours simulated: " +str(toursTotal)+"\nTotal pans dropped: "+str(simsRan)+"\nFrom this data, the pan drop rate was: "+str((simsRan/toursTotal)*100)+"%"+". The actual drop rate is 0.01%."+"\nSimulations that ended in profit: "+str(profit)+"\nSimulations that ended in loss: "+str(loss)+"\n\nBest run:\nTour that pan dropped on: " + str(lowestTour)+"\nMoney spent on " + str(lowestTour) + " tours: $"+str(cost)+", you made $" + str(currentPanPrice - cost) + " in profit.\nOdds of getting a pan on your "+str(lowestTour)+" run: "+str(odds)+"%")
+if round(float(lowestTour*(price*int(ticketsPerTour)))) < currentPanPrice:
+    print("\nSim finished!\n\nTotal tours simulated: " +str(toursTotal)+"\nTotal pans dropped: "+str(simsRan)+"\nFrom this data, the pan drop rate was: "+str((simsRan/toursTotal)*100)+"%"+". The actual drop rate is 0.01%."+"\nSimulations that ended in profit: "+str(profit)+"\nSimulations that ended in loss: "+str(loss)+"\nThe chances of earning profit would be: "+str((profit/loss)*100)+"%\n\nBest run:\nTour that pan dropped on: " + str(lowestTour)+"\nMoney spent on " + str(lowestTour) + " tours: $"+str(cost)+", you made $" + str(currentPanPrice - cost) + " in profit.\nOdds of getting a pan on your "+str(lowestTour)+" run: "+str(odds)+"%")
 else:
-    print("\nSim finished!\n\nTotal tours simulated: " +str(toursTotal)+"\nTotal pans dropped: "+str(simsRan)+"\nFrom this data, the pan drop rate was: "+str((simsRan/toursTotal)*100)+"%"+". The actual drop rate is 0.01%."+"\nSimulations that ended in profit: "+str(profit)+"\nSimulations that ended in loss: "+str(loss)+"\n\nBest run:\nTour that pan dropped on: " + str(lowestTour)+"\nMoney spent on " + str(lowestTour) + " tours: $"+str(cost)+", you lost $" + str(currentPanPrice - cost) + " in profit.\nOdds of getting a pan on your "+str(lowestTour)+" run: "+str(odds)+"%")
+    print("\nSim finished!\n\nTotal tours simulated: " +str(toursTotal)+"\nTotal pans dropped: "+str(simsRan)+"\nFrom this data, the pan drop rate was: "+str((simsRan/toursTotal)*100)+"%"+". The actual drop rate is 0.01%."+"\nSimulations that ended in profit: "+str(profit)+"\nSimulations that ended in loss: "+str(loss)+"\nThe chances of earning profit would be: "+str((profit/loss)*100)+"%\n\nBest run:\nTour that pan dropped on: " + str(lowestTour)+"\nMoney spent on " + str(lowestTour) + " tours: $"+str(cost)+", you lost $" + str(currentPanPrice - cost) + " in profit.\nOdds of getting a pan on your "+str(lowestTour)+" run: "+str(odds)+"%")
 
 a=input()
